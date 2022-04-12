4d9615b4893e640535f04587bffc1712fbaf6b46
Geovanne <callegarog00@gmail.com>
2022-04-12 20:05:53 -0300

    adding redux-persist to save user data on reload (local storage)

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './store/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistedStore} from './store/store'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

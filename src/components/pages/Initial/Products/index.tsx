21e11a13fcdf499290b2c99fe1bc93a04ed2ef4f
Geovanne <callegarog00@gmail.com>
2022-04-12 13:26:52 -0300

    putting the products through the mock on the main page

--- src/components/pages/Initial/Products/index.tsx
+++ src/components/pages/Initial/Products/index.tsx
@@ -42,75 +42,26 @@ export const Products = () => {
     return (
         <div className='containerProducts'>
             <div className='sliderContainerProducts'>
-                <div className='imageContainer'>
-                    <div className='visualization' id='one' data-initial>
-                        <p>Visualização rápida</p>
+                {products.map((product: any) => (
+                    <div className='imageContainer'>
+                        <div className='visualization' data-initial={product.id}>
+                            <p>Visualização rápida</p>
+                        </div>
+                        <img 
+                            src={product.src}
+                            alt={product.id}
+                            onMouseEnter={(event) => showVisualization(event.target)} 
+                            onMouseLeave={removeVisualization}
+                            onClick={() => handleToggle(product.id)}
+                            className='imagemInitial'
+                            data-initial={product.id}
+                        />
+                        <div className='textImageContainer'>
+                            <h3>{product.name}</h3>
+                            <p>R$ {product.price}</p>
+                        </div>
                     </div>
-                    <img 
-                        src={products[0].src}
-                        alt={products[0].id}
-                        onMouseEnter={(event) => showVisualization(event.target)} 
-                        onMouseLeave={removeVisualization} 
-                        onClick={() => handleToggle(products[0].id)}
-                        className='imagemInitial'
-                        data-initial
-                    />
-                    <div className='textImageContainer'>
-                        <h3>{products[0].name}</h3>
-                        <p>R$ {products[0].price}</p>
-                    </div>
-                </div>
-                <div className='imageContainer'>
-                    <div className='visualization' id='two' data-initial='2'>
-                        <p>Visualização rápida</p>
-                    </div>
-                    <img 
-                        src="https://static.wixstatic.com/media/157553_477528c34660447a8a0afdbdf1a0fe05.jpg/v1/fill/w_500,h_500,al_c,q_85,usm_0.66_1.00_0.01/157553_477528c34660447a8a0afdbdf1a0fe05.webp" 
-                        alt="" 
-                        onMouseEnter={(event) => showVisualization(event.target)} 
-                        onMouseLeave={removeVisualization}
-                        className="imagemInitial"
-                        data-initial='2'    
-                    />
-                    <div className='textImageContainer'>
-                        <h3>Eu sou um produto!</h3>
-                        <p>R$19.99</p>
-                    </div>
-                </div>
-                <div className='imageContainer'>
-                    <div className='visualization' id='three' data-initial='3'>
-                        <p>Visualização rápida</p>
-                    </div>
-                    <img 
-                        src="https://static.wixstatic.com/media/157553_693426837c2f455693946bd5ff85e975.jpg/v1/fill/w_500,h_500,al_c,q_85,usm_0.66_1.00_0.01/157553_693426837c2f455693946bd5ff85e975.webp" 
-                        alt="" 
-                        onMouseEnter={(event) => showVisualization(event.target)} 
-                        onMouseLeave={removeVisualization} 
-                        className='imagemInitial'
-                        data-initial='3'
-                    />
-                    <div className='textImageContainer'>
-                        <h3>Eu sou um produto!</h3>
-                        <p>R$19.99</p>
-                    </div>
-                </div>
-                <div className='imageContainer'>
-                    <div className='visualization' id='four' data-initial='4'>
-                        <p>Visualização rápida</p>
-                    </div>
-                    <img 
-                        src="https://static.wixstatic.com/media/157553_abfd11f66c2a446595e5c09ce463b4b7.jpg/v1/fill/w_500,h_500,al_c,q_85,usm_0.66_1.00_0.01/157553_abfd11f66c2a446595e5c09ce463b4b7.webp" 
-                        alt="" 
-                        onMouseEnter={(event) => showVisualization(event.target)} 
-                        onMouseLeave={removeVisualization}
-                        className="imagemInitial"
-                        data-initial='4' 
-                    />
-                    <div className='textImageContainer'>
-                        <h3>Eu sou um produto!</h3>
-                        <p>R$19.99</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
